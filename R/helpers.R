b283e7160ddfa073fe2291e5242b16b22516d8a3
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-27 08:45:12 -0500

    `purrr::flatten` removes date class, use `do.call(c,x)` instead

--- R/helpers.R
+++ R/helpers.R
@@ -93,9 +93,7 @@ hud_last_updated <- function(x, path = "data") {
                 . == 1 ~ file.info(x)$mtime,
                 . > 1 ~ do.call(c, purrr::map(rlang::set_names(x), ~file.info(.x)$mtime)) |> sort(decreasing = TRUE))
   } else {
-    UU::list.files2(path) |>
-      purrr::map(purrr::possibly(hud_last_updated, lubridate::NA_POSIXct_), path = path) |>
-      purrr::flatten()
+    do.call(c, purrr::map(UU::list.files2(path), purrr::possibly(hud_last_updated, lubridate::NA_POSIXct_), path = path))
   }
 
 }
