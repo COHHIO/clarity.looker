6ea21ff703b7e6ea54030fafbe09eb0d3038decd
Trevin Flickinger <twflick@gmail.com>
2024-08-08 13:33:21 -0400

    update documentation

--- R/aaa_imports.R
+++ R/aaa_imports.R
@@ -1,5 +1,6 @@
 #' @title Reimports
 #' @description Infix functions from other packages used in clarity.looker.
+#' @name clarity_looker_reimports
 #' @importFrom rlang `%||%`
 #' @importFrom dplyr `%>%`
 #' @importFrom lubridate `.__T__-:base`
